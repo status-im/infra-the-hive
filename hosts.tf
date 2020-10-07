bacb5dd068bad4f69d42bc447567b762e655254a
Jakub Sokołowski <jakub@status.im>
2020-10-07 16:42:55 +0200

    deploy The Hive master node and 3 slave nodes for storage
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

---
+++ hosts.tf
@@ -0,0 +1,27 @@
+/* Frontend application */
+module "hive_master" {
+  source        = "github.com/status-im/infra-tf-google-cloud"
+  host_count    = 1
+  name          = "master"
+  env           = "thehive"
+  group         = "thehive-master"
+  type          = "n1-standard-2"
+  root_vol_size = 40
+  domain        = var.hosts_domain
+
+  /* Application Web UI */
+  open_tcp_ports = [ "80", "443" ]
+}
+
+/* Backend storage */
+module "hive_slave" {
+  source        = "github.com/status-im/infra-tf-google-cloud"
+  host_count    = 3
+  name          = "slave"
+  env           = "thehive"
+  group         = "thehive-slave"
+  type          = "n1-standard-4"
+  root_vol_size = 30
+  data_vol_size = 30
+  domain        = var.hosts_domain
+}
