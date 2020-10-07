0ae38e2608362e2b5043c2234bc4f31434794108
Jakub Sokołowski <jakub@status.im>
2020-10-07 14:21:45 +0200

    add versions.tf
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

---
+++ versions.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_version = "~> 0.13.3"
+  required_providers {
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = " = 2.10.1"
+    }
+    google = {
+      source  = "hashicorp/google"
+      version = " = 3.40.0"
+    }
+    ansible = {
+      source  = "nbering/ansible"
+      version = " = 1.0.4"
+    }
+    pass = {
+      source  = "camptocamp/pass"
+      version = " = 1.4.0"
+    }
+  }
+}
