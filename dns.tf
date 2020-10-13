fda79b60666d9b0ca144a9d8f7060bc6384e3e26
Jakub Sokołowski <jakub@status.im>
2020-10-13 16:35:59 +0200

    rename move thehive-es under infra.status.im
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- dns.tf
+++ dns.tf
@@ -19,7 +19,7 @@ resource "cloudflare_record" "cortex_ui" {
 resource "cloudflare_record" "hive_es_hq" {
   zone_id = lookup(local.zones, "status.im")
   type    = "A"
-  name    = "thehive-es"
+  name    = "thehive-es.infra"
   value   = module.hive_slave.public_ips[count.index]
   count   = length(module.hive_slave.public_ips)
   proxied = true
