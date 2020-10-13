9ef85ceeb4d2f4ff7c5a12869c86916926e76446
Jakub Sokołowski <jakub@status.im>
2020-10-13 12:50:56 +0200

    configure an Nginx proxy for cortex.status.im
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- dns.tf
+++ dns.tf
@@ -7,6 +7,15 @@ resource "cloudflare_record" "hive_ui" {
   proxied = true
 }
 
+resource "cloudflare_record" "cortex_ui" {
+  zone_id = lookup(local.zones, "status.im")
+  type    = "A"
+  name    = "cortex"
+  value   = module.hive_master.public_ips[count.index]
+  count   = length(module.hive_master.public_ips)
+  proxied = true
+}
+
 resource "cloudflare_record" "hive_es_hq" {
   zone_id = lookup(local.zones, "status.im")
   type    = "A"
